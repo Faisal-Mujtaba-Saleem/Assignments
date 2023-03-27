41e26d1881994ba0a3447213057fa6ee2b2ad08e
Faisal-Mujtaba-Saleem <125788844+Faisal-Mujtaba-Saleem@users.noreply.github.com>
2023-03-27 09:31:39 +0500

    Add files via upload

---
+++ Assignment-4 (Js)/Task_3/index.js
@@ -0,0 +1,6 @@
+// Formula to convert the temprature from Fahrenheit to Celcius:
+// C = (°F - 32) × 5 / 9
+
+let tempInFahrenheit = 72;
+let tempInCelcius = (tempInFahrenheit - 32) * (5 / 9);
+console.log(tempInCelcius);
